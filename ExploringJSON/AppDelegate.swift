55e3d37783be6bd36b0ae47568ffa2c4914dc8be
AnnaSemichaeva <tdhnss@gmail.com>
2022-12-15 07:02:55 +0300

    Initial commit

---
+++ ExploringJSON/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ExploringJSON
+//
+//  Created by macuser on 12/15/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
