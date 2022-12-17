e060f041cf9a20ee9ea3732339c39896c7d74368
AnnaSemichaeva <tdhnss@gmail.com>
2022-12-17 04:16:22 +0300

    minor changes

--- ExploringJSON/SampleJSONPartsByHand/SampleJSONPartsByHandManager.swift
+++ ExploringJSON/SampleJSONPartsByHand/SampleJSONPartsByHandManager.swift
@@ -15,23 +15,67 @@ class SampleJSONPartsByHandManager {
     func samplejsonPartsByHand() {
         guard let path = Bundle.main.path(forResource: "Sample", ofType: "json") else {
             return }
-        let age_limit = Age_limit()
+        let age_limit1 = Age_limit()
         let attributes = Attributes()
         let promomedia = Promomedia()
-        let materials = Materials()
+        //let something = Facets()
+        //let materials = Materials()
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
             let jsonSerialization = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
             print()
             if let json = jsonSerialization as? [String: Any] {
-                guard let age_limit = json["age_limit"] as? [String: Any] else {
+                
+                if let age_limit0 = json["Sample"] as? Int {
+                    age_limit1.age_limit = age_limit0
+                }
+                
+                guard let attributes1 = json["Sample"] as? [String: Any] else {
                     return
                 }
+                
+                if let facets = attributes1["facets"] as? Facets {
+                    attributes.facets = facets
+                }
+                
+                if let next = attributes1["next"] as? Array<Any> {
+                    attributes.next = next
+                }
+                
+//                guard let age_limit = json["Sample"] as? [String: Any] else {
+//                    return
+//                }
+//
+//                if let age_limit0 = age_limit["age_limit"] as? Int {
+//                    age_limit1.age_limit = age_limit0
+//                }
+//
+//                guard let attributes1 = json["Sample"] as? [String: Any] else {
+//                    return
+//                }
+                
+                //newTaskSport.q1 = task
+                //something.
+                
+                
+//                guard let facets = attributes1["facets"] as? [String: Any] else {
+//                   return
+//                }
+                
+//                if let age_limit0 = age_limit["age_limit"] as? Int {
+//                    age_limit1.age_limit = age_limit0
+//                }
+                
+//                if let options = qOne["options"] as? [String] {
+//                    task.options = options
+//                }
+                
+                //discipline.maths = newTaskMaths
+                //discipline.sport = newTaskSport
+                //attributes.facets = something
+                age_limit1.attributes = attributes
+                print(age_limit1)
             }
-            //discipline.maths = newTaskMaths
-            //discipline.sport = newTaskSport
-            //quiz.quiz = discipline
-            //print(quiz)
         } catch {
             print("Data err")
         }
