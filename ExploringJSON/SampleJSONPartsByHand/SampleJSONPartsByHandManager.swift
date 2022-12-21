ccfd805261d771c9db0fe600112f8a79926ee777
AnnaSemichaeva <tdhnss@gmail.com>
2022-12-21 17:17:26 +0300

    minor chamges

--- ExploringJSON/SampleJSONPartsByHand/SampleJSONPartsByHandManager.swift
+++ ExploringJSON/SampleJSONPartsByHand/SampleJSONPartsByHandManager.swift
@@ -30,116 +30,116 @@ class SampleJSONPartsByHandManager {
             
             if let json = jsonSerialization as? [String: Any] {
                 
-                var i = 0
-                for key in json.keys {
-                    print("[\(i)] = \(key)")
-                    i += 1
-                }
+//                var i = 0
+//                for key in json.keys {
+//                    print("[\(i)] = \(key)")
+//                    i += 1
+//                }
                 
                 if let age_limit0 = json["age_limit"] as? Int {
                     age_limit1.age_limit = age_limit0
-                    print(">> age_limit0: \(age_limit0)")
+                    //print(">> age_limit0: \(age_limit0)")
                 } else {
-                    print(">> GG no age limit")
+                    //print(">> GG no age limit")
                 }
                 
-                if let bucket = json["Sample"] as? String {
+                if let bucket = json["bucket"] as? String {
                     age_limit1.bucket = bucket
                 }
                 
-                if let container_type = json["Sample"] as? String {
+                if let container_type = json["container_type"] as? String {
                     age_limit1.container_type = container_type
                 }
                 
-                if let content_origin = json["Sample"] as? Int {
+                if let content_origin = json["content_origin"] as? Int {
                     age_limit1.content_origin = content_origin
                 }
                 
-                if let content_type = json["Sample"] as? String {
+                if let content_type = json["content_type"] as? String {
                     age_limit1.container_type = content_type
                 }
                 
-                if let dob_required = json["Sample"] as? Bool {
+                if let dob_required = json["dob_required"] as? Bool {
                     age_limit1.dob_required = dob_required
                 }
                 
-                if let game_contentType = json["Sample"] as? String {
+                if let game_contentType = json["game_contentType"] as? String {
                     age_limit1.game_contentType = game_contentType
                 }
                 
-                if let id = json["Sample"] as? String {
+                if let id = json["id"] as? String {
                     age_limit1.id = id
                 }
                 
-                if let legal_text = json["Sample"] as? String {
+                if let legal_text = json["legal_text"] as? String {
                     age_limit1.legal_text = legal_text
                 }
                 
-                if let links = json["Sample"] as? [Int] {
+                if let links = json["links"] as? [Int] {
                     age_limit1.links = links
                 }
                 
-                if let long_desc = json["Sample"] as? String {
+                if let long_desc = json["long_desc"] as? String {
                     age_limit1.long_desc = long_desc
                 }
                 
-                if let name = json["Sample"] as? String {
+                if let name = json["name"] as? String {
                     age_limit1.name = name
                 }
                 
-                if let pageTypeId = json["Sample"] as? Int {
+                if let pageTypeId = json["pageTypeId"] as? Int {
                     age_limit1.pageTypeId = pageTypeId
                 }
                 
-                if let provider_name = json["Sample"] as? String {
+                if let provider_name = json["provider_name"] as? String {
                     age_limit1.provider_name = provider_name
                 }
                 
-                if let release_date = json["Sample"] as? String {
+                if let release_date = json["release_date"] as? String {
                     age_limit1.release_date = release_date
                 }
                 
-                if let restricted = json["Sample"] as? Bool {
+                if let restricted = json["restricted"] as? Bool {
                     age_limit1.restricted = restricted
                 }
                 
-                if let revision = json["Sample"] as? Int {
+                if let revision = json["revision"] as? Int {
                     age_limit1.revision = revision
                 }
                 
-                if let short_name = json["Sample"] as? String {
+                if let short_name = json["short_name"] as? String {
                     age_limit1.short_name = short_name
                 }
                 
-                if let size = json["Sample"] as? Int {
+                if let size = json["size"] as? Int {
                     age_limit1.size = size
                 }
                 
-                if let sku_links = json["Sample"] as? [Int] {
+                if let sku_links = json["sku_links"] as? [Int] {
                     age_limit1.sku_links = sku_links
                 }
                 
-                if let sort = json["Sample"] as? String {
+                if let sort = json["sort"] as? String {
                     age_limit1.sort = sort
                 }
                 
-                if let start = json["Sample"] as? Int {
+                if let start = json["start"] as? Int {
                     age_limit1.start = start
                 }
                 
-                if let timestamp = json["Sample"] as? Int {
+                if let timestamp = json["timestamp"] as? Int {
                     age_limit1.timestamp = timestamp
                 }
                 
-                if let title_name = json["Sample"] as? String {
+                if let title_name = json["title_name"] as? String {
                     age_limit1.title_name = title_name
                 }
                 
-                if let top_category = json["Sample"] as? String {
+                if let top_category = json["top_category"] as? String {
                     age_limit1.top_category = top_category
                 }
                 
-                if let total_results = json["Sample"] as? Int {
+                if let total_results = json["total_results"] as? Int {
                     age_limit1.total_results = total_results
                 }
                 
